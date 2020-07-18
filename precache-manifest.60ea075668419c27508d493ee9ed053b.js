38e588895d513b8554afb57a245cf051dbfb8fe3
amalnj4858 <amalnj4858@users.noreply.github.com>
2020-07-18 19:09:09 +0530

    Updates

---
+++ precache-manifest.60ea075668419c27508d493ee9ed053b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4bab7a6d8a8d148921da947a0fea68c",
+    "url": "/ISSLocationTracker/index.html"
+  },
+  {
+    "revision": "6365805846f977bbfb2e",
+    "url": "/ISSLocationTracker/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "045e549971010dfa5ec6",
+    "url": "/ISSLocationTracker/static/css/main.60e9e38a.chunk.css"
+  },
+  {
+    "revision": "6365805846f977bbfb2e",
+    "url": "/ISSLocationTracker/static/js/2.67ac08c4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ISSLocationTracker/static/js/2.67ac08c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "045e549971010dfa5ec6",
+    "url": "/ISSLocationTracker/static/js/main.c23ce601.chunk.js"
+  },
+  {
+    "revision": "9bc5d8ef82d2098b4d90",
+    "url": "/ISSLocationTracker/static/js/runtime-main.a0992e51.js"
+  }
+]);
